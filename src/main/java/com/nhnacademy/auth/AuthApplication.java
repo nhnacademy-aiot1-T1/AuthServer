7a03f674ee5f57e4021a3e8c1da4a40f14e83ee4
chanhwiim <chanhwiim459@gmail.com>
2024-03-31 01:09:31 +0900

    fix: DataSource error 발생. 처리.

--- src/main/java/com/nhnacademy/auth/AuthApplication.java
+++ src/main/java/com/nhnacademy/auth/AuthApplication.java
@@ -2,10 +2,10 @@ package com.nhnacademy.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
-@EnableConfigurationProperties
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AuthApplication {
 
 	public static void main(String[] args) {
