3f496d5a4aa9909bed7a656f89463c66a27699e1
chanhwiim <chanhwiim459@gmail.com>
2024-04-01 13:42:18 +0900

    fix: dependency 수정으로 인한 exclude 가용 부분 제거. 수

--- src/main/java/com/nhnacademy/auth/AuthApplication.java
+++ src/main/java/com/nhnacademy/auth/AuthApplication.java
@@ -2,10 +2,8 @@ package com.nhnacademy.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AuthApplication {
 
 	public static void main(String[] args) {
