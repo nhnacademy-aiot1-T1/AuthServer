3f3010174de862c82f9bb1ae547d2e5583c78419
{qaw302} <{osshun21@gmail.com}>
2024-04-02 21:50:51 +0900

    fix : LoginResponse @Getter 추가

--- src/main/java/com/nhnacademy/auth/dto/LoginResponse.java
+++ src/main/java/com/nhnacademy/auth/dto/LoginResponse.java
@@ -1,10 +1,12 @@
 package com.nhnacademy.auth.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Login Request 대한 Response Body 내용
  */
+@Getter
 @AllArgsConstructor
 public class LoginResponse {
     String userId;
