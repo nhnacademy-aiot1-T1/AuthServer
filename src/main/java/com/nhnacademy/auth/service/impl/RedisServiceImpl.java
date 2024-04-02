72404f7bfb5087a6bf90e2d65368641e57b73633
chanhwiim <chanhwiim459@gmail.com>
2024-04-02 14:16:27 +0900

    docs: add javadoc

--- src/main/java/com/nhnacademy/auth/service/impl/RedisServiceImpl.java
+++ src/main/java/com/nhnacademy/auth/service/impl/RedisServiceImpl.java
@@ -7,17 +7,27 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
+/**
+ * Redis template의 param = key (String), value (String)
+ *
+ */
 @Service
 public class RedisServiceImpl implements RedisService {
     private RedisTemplate<String, String> redisTemplate;
 
+    /**
+     * TTL 기능이 있음. timeDuration으로 설정 가능. 단위: sec
+     * @param refreshToken
+     * @param userId
+     * @param timeDuration : sec
+     */
     @Override
     public void save(String refreshToken, String userId, Duration timeDuration) {
         redisTemplate.opsForValue().set(refreshToken, userId, timeDuration);
     }
 
     @Override
-    public String findRefreshTokenByUserId(String refreshToken) {
+    public String findUserIdByRefreshToken(String refreshToken) {
         return redisTemplate.opsForValue().get(refreshToken);
     }
 
