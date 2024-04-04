febef6d9380fab8cd2afdc54b076cdb4e49e1a65
chanhwiim <chanhwiim459@gmail.com>
2024-04-04 21:04:34 +0900

    feat: add exception, docs: javadoc

--- src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
+++ src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
@@ -1,6 +1,7 @@
 package com.nhnacademy.auth.advice;
 
 import com.nhnacademy.auth.dto.CommonResponse;
+import com.nhnacademy.auth.exception.IpIsNotEqualsException;
 import com.nhnacademy.auth.exception.PasswordNotMatchException;
 import com.nhnacademy.auth.exception.RefreshTokenNotFoundException;
 import com.nhnacademy.auth.exception.UserIdNotFoundException;
@@ -9,29 +10,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
-
+/**
+ * 현재 exception handler만 하는 RestControllerAdvice.
+ * <p>
+ * 3종의 예외 핸들링 중.
+ * </p>
+ */
 @RestControllerAdvice
 public class CommonAdvice {
 
   @ExceptionHandler(PasswordNotMatchException.class)
-  public ResponseEntity<CommonResponse<PasswordNotMatchException>> passwordNotMatch(
-      PasswordNotMatchException passwordNotMatchException) {
+  public ResponseEntity<CommonResponse<PasswordNotMatchException>> passwordNotMatch() {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
         .body(CommonResponse.fail("password가 일치하지 않습니다."));
   }
 
-  @ExceptionHandler(RefreshTokenNotFoundException.class)
-  public ResponseEntity<CommonResponse<RefreshTokenNotFoundException>> refreshTokenNotFound(
-      RefreshTokenNotFoundException refreshTokenNotFoundException) {
+  @ExceptionHandler(UserIdNotFoundException.class)
+  public ResponseEntity<CommonResponse<UserIdNotFoundException>> userIdNotFound() {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-        .body(CommonResponse.fail("refresh token의 인증에 실패하여, access token의 생성이 불가능합니다."));
+        .body(CommonResponse.fail("아이디를 찾을 수 없습니다."));
   }
 
-  @ExceptionHandler(UserIdNotFoundException.class)
-  public ResponseEntity<CommonResponse<UserIdNotFoundException>> userIdNotFound(
-      UserIdNotFoundException userIdNotFoundException) {
+  @ExceptionHandler(IpIsNotEqualsException.class)
+  public ResponseEntity<CommonResponse<IpIsNotEqualsException>> accessTokenIsNotEqual() {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-        .body(CommonResponse.fail("아이디를 찾을 수 없습니다.."));
+        .body(CommonResponse.fail("access token이 일치하지 않습니다."));
   }
 }
