47ae6e163736cb96dd950da1218b3b9652adbeae
chanhwiim <chanhwiim459@gmail.com>
2024-04-04 21:09:32 +0900

    build : deprecated

--- src/main/java/com/nhnacademy/auth/exception/RefreshTokenNotFoundException.java
+++ src/main/java/com/nhnacademy/auth/exception/RefreshTokenNotFoundException.java
@@ -1,7 +1,12 @@
 package com.nhnacademy.auth.exception;
 
+/**
+ * refresh token을 찾을 수 경우, 발생하는 예외. 지금은 사용하지 않음.
+ */
+@Deprecated
 public class RefreshTokenNotFoundException extends RuntimeException {
-    public RefreshTokenNotFoundException(String refreshToken) {
-        super(refreshToken + "을 찾을 수 없습니다.");
-    }
+
+  public RefreshTokenNotFoundException(String refreshToken) {
+    super(refreshToken + "을 찾을 수 없습니다.");
+  }
 }
