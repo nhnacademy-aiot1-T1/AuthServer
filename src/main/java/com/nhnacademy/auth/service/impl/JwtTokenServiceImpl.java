4f28d42ff6c143b745da5eccd4c60c77c4299193
chanhwiim <chanhwiim459@gmail.com>
2024-04-04 21:06:42 +0900

    feat: add token parsing change regenerateAccessToken, docs: javadoc

--- src/main/java/com/nhnacademy/auth/service/impl/JwtTokenServiceImpl.java
+++ src/main/java/com/nhnacademy/auth/service/impl/JwtTokenServiceImpl.java
@@ -1,89 +1,115 @@
 package com.nhnacademy.auth.service.impl;
 
-import com.nhnacademy.auth.exception.RefreshTokenNotFoundException;
+import com.nhnacademy.auth.dto.User;
+import com.nhnacademy.auth.exception.IpIsNotEqualsException;
 import com.nhnacademy.auth.properties.JwtProperties;
+import com.nhnacademy.auth.service.IpGeolationService;
 import com.nhnacademy.auth.service.JwtTokenService;
-import com.nhnacademy.auth.service.RedisService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.time.DateUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.Date;
 
 /**
  * UserId를 받아, JwtToken을 발급하는 Service layer
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JwtTokenServiceImpl implements JwtTokenService {
 
-    private final JwtProperties jwtProperties;
+  private final JwtProperties jwtProperties;
 
-    private final RedisService redisService;
+  private final IpGeolationService ipGeolationService;
 
-    private static final int EXPIRED_TIME_MINUTE = 5;
-    private static final int REFRESH_TOKEN_EXPIRED_TIME_WEEK = 1;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
+  private static final int EXPIRED_TIME_MINUTE = 5;
 
 
-    @Override
-    public String generateAccessToken(String userId) {
-        return createJwtToken(userId, EXPIRED_TIME_MINUTE);
+  @Override
+  public String generateAccessToken(User user, String ip) {
+    return createJwtToken(user.getId(), user.getRole().toString(), ip, EXPIRED_TIME_MINUTE);
+  }
+
+  /**
+   * 실질적인 발급 로직.
+   *
+   * @param userId      : token 발급 대상.
+   * @param expiredTime : 만료 시간. 토큰의 종류에 따라 만료 시간이 다름.
+   * @return string : JwtToken
+   */
+  @Override
+  public String createJwtToken(String userId, String userRole, String ip, int expiredTime) {
+    String encodeIp = bCryptPasswordEncoder.encode(ip);
+
+    Claims claims = Jwts.claims();
+    claims.put("userId", userId);
+    claims.put("userRole", userRole);
+    claims.put("userIp", encodeIp);
+
+    log.error("claims", claims);
+    Date now = new Date();
+
+    return Jwts.builder()
+        .setClaims(claims)
+        .setIssuedAt(now)
+        .setExpiration(DateUtils.addMinutes(now, expiredTime))
+        .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+        .compact();
+  }
+
+  @Override
+  public String generateJwtTokenFromMobile(User user, String ip) {
+    String mobileIpCountry = ipGeolationService.getContury(ip);
+    return generateAccessToken(user, mobileIpCountry);
+  }
+
+  @Override
+  public String regenerateAccessToken(String nowIp, String legacyAccessToken) {
+    String userId = getUserIdFromJwtToken(legacyAccessToken);
+    String userRole = getUserRoleFromJwtToken(legacyAccessToken);
+    String userIp = getEncodeIpFromJwtToken(legacyAccessToken);
+
+    if (checkAccessTokenIp(userIp, nowIp)) {
+      return createJwtToken(userId, userRole, userIp, EXPIRED_TIME_MINUTE);
     }
+    throw new IpIsNotEqualsException();
+  }
 
-    /**
-     * refresh token이 발급 된다면, 7일 후 자동적으로 삭제되게 TTL 설정.
-     * @param userId
-     * @return refresh token
-     */
-    @Override
-    public String generateRefreshToken(String userId) {
-        String refreshToken = createJwtToken(userId, REFRESH_TOKEN_EXPIRED_TIME_WEEK);
-        redisService.save(refreshToken, userId, Duration.ofDays(7));
-        return refreshToken;
-    }
+  private boolean checkAccessTokenIp(String legacyIp, String nowIp) {
+    return legacyIp.equals(nowIp);
+  }
 
-    /**
-     * 실질적인 발급 로직.
-     * @param userId : token 발급 대상.
-     * @param expiredTime : 만료 시간. 토큰의 종류에 따라 만료 시간이 다름.
-     * @return string : JwtToken
-     */
-    @Override
-    public String createJwtToken(String userId, int expiredTime) {
-        Claims claims = Jwts.claims();
-        claims.put("userId", userId);
+  private String getEncodeIpFromJwtToken(String token) {
+    Claims claims = Jwts.parser()
+        .setSigningKey(jwtProperties.getSecret())
+        .parseClaimsJws(token)
+        .getBody();
 
-        Date now = new Date();
+    return claims.get("userIp", String.class);
+  }
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(DateUtils.addMinutes(now, expiredTime))
-                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
-                .compact();
-    }
+  private String getUserIdFromJwtToken(String token) {
+    Claims claims = Jwts.parser()
+        .setSigningKey(jwtProperties.getSecret())
+        .parseClaimsJws(token)
+        .getBody();
 
-    /**
-     * redis에 refreshToken이 있는지 찾은 후, 유무에 따라 return이 달라진다.
-     * @param refreshToken : 검색 대상
-     * @exception RefreshTokenNotFoundException(String) : refresh이 redis에서 찾을 수 없을 경우.
-     * @return accessToken
-     */
-    @Override
-    public String regenerateAccessToken(String refreshToken) {
-        if (checkRefreshToken(refreshToken)) {
-            String getUserIdFromRedis = redisService.findUserIdByRefreshToken(refreshToken);
-            return generateAccessToken(getUserIdFromRedis);
-        }
-        throw new RefreshTokenNotFoundException(refreshToken);
-    }
+    return claims.get("userId", String.class);
+  }
 
-    @Override
-    public boolean checkRefreshToken(String refreshToken) {
-        return redisService.haveThisKey(refreshToken);
-    }
+  private String getUserRoleFromJwtToken(String token) {
+    Claims claims = Jwts.parser()
+        .setSigningKey(jwtProperties.getSecret())
+        .parseClaimsJws(token)
+        .getBody();
+
+    return claims.get("userRole", String.class);
+  }
 }
