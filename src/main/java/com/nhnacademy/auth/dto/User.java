081e023b392c58e9533331b3c27c7888a0e6a7e9
chanhwiim <chanhwiim459@gmail.com>
2024-04-04 21:10:13 +0900

    feat : add allargConstructor

--- src/main/java/com/nhnacademy/auth/dto/User.java
+++ src/main/java/com/nhnacademy/auth/dto/User.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Account api에서 받을 user 정보
  */
 @Getter
+@AllArgsConstructor
 public class User {
     String id;
     UserStatus status;
