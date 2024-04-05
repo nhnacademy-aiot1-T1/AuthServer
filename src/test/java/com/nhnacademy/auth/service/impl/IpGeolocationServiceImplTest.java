e0ecc8401993aa949af690c1acb6afad9d4faec2
chanhwiim <chanhwiim459@gmail.com>
2024-04-05 20:31:33 +0900

    test: IpGeolocationServiceImplTest

---
+++ src/test/java/com/nhnacademy/auth/service/impl/IpGeolocationServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.nhnacademy.auth.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class IpGeolocationServiceImplTest {
+
+  @Autowired
+  private IpGeolocationServiceImpl ipGeolocationService;
+
+  private String ip;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    ip = "112.216.11.34";
+  }
+
+  @Test
+  void getContury() throws IOException, GeoIp2Exception {
+    String i1p = ipGeolocationService.getContury(ip);
+    log.info(":{}", i1p);
+    assertNotNull(ipGeolocationService.getContury(ip));
+  }
+}
