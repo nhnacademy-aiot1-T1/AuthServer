704ab604f610e0b29417cdff3006342326acbee6
chanhwiim <chanhwiim459@gmail.com>
2024-04-05 20:38:44 +0900

    docs: add javadoc

--- src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
+++ src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.auth.advice;
 
+import com.nhnacademy.auth.exception.AccessTokenNotFoundException;
 import com.nhnacademy.auth.exception.IpIsNotEqualsException;
 import com.nhnacademy.auth.exception.PasswordNotMatchException;
 import com.nhnacademy.auth.exception.UserIdNotFoundException;
@@ -12,27 +13,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * 현재 exception handler만 하는 RestControllerAdvice.
  * <p>
- * 3종의 예외 핸들링 중.
+ * RuntimeException으로 통일. httpStatus가 401로 동일.
+ * <li>4종의 예외 핸들링 중.
  * </p>
  */
 @RestControllerAdvice
 public class CommonAdvice {
 
-  @ExceptionHandler(PasswordNotMatchException.class)
-  public ResponseEntity<CommonResponse<PasswordNotMatchException>> passwordNotMatch() {
+  @ExceptionHandler(value = {PasswordNotMatchException.class,
+      UserIdNotFoundException.class,
+      IpIsNotEqualsException.class,
+      AccessTokenNotFoundException.class})
+  public ResponseEntity<CommonResponse<RuntimeException>> passwordNotMatch(RuntimeException e) {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-        .body(CommonResponse.fail("password가 일치하지 않습니다."));
-  }
-
-  @ExceptionHandler(UserIdNotFoundException.class)
-  public ResponseEntity<CommonResponse<UserIdNotFoundException>> userIdNotFound() {
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-        .body(CommonResponse.fail("아이디를 찾을 수 없습니다."));
-  }
-
-  @ExceptionHandler(IpIsNotEqualsException.class)
-  public ResponseEntity<CommonResponse<IpIsNotEqualsException>> accessTokenIsNotEqual() {
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-        .body(CommonResponse.fail("access token이 일치하지 않습니다."));
+        .body(CommonResponse.fail(e.getMessage()));
   }
 }
