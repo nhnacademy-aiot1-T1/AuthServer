f7395a1f57102a3ece70a41aca97af12e6408a62
chanhwiim <chanhwiim459@gmail.com>
2024-04-05 22:03:41 +0900

    feat: add 1 exception, docs: change javadoc

--- src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
+++ src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
@@ -5,6 +5,7 @@ import com.nhnacademy.auth.exception.IpIsNotEqualsException;
 import com.nhnacademy.auth.exception.PasswordNotMatchException;
 import com.nhnacademy.auth.exception.UserIdNotFoundException;
 import com.nhnacademy.common.dto.CommonResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * 현재 exception handler만 하는 RestControllerAdvice.
  * <p>
  * RuntimeException으로 통일. httpStatus가 401로 동일.
- * <li>4종의 예외 핸들링 중.
+ * <li>5종의 예외 핸들링 중.
  * </p>
  */
 @RestControllerAdvice
@@ -23,7 +24,8 @@ public class CommonAdvice {
   @ExceptionHandler(value = {PasswordNotMatchException.class,
       UserIdNotFoundException.class,
       IpIsNotEqualsException.class,
-      AccessTokenNotFoundException.class})
+      AccessTokenNotFoundException.class,
+      ExpiredJwtException.class})
   public ResponseEntity<CommonResponse<RuntimeException>> passwordNotMatch(RuntimeException e) {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
         .body(CommonResponse.fail(e.getMessage()));
