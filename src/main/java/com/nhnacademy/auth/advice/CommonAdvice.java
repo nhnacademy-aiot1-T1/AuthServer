48dc38214bf984afc96b1ee7c299de7912d4ab81
chanhwiim <chanhwiim459@gmail.com>
2024-04-06 18:14:56 +0900

    feat: JsonProcessingException 핸들링 추가.

--- src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
+++ src/main/java/com/nhnacademy/auth/advice/CommonAdvice.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.auth.advice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nhnacademy.auth.exception.AccessTokenNotFoundException;
 import com.nhnacademy.auth.exception.IpIsNotEqualsException;
 import com.nhnacademy.auth.exception.PasswordNotMatchException;
@@ -15,7 +16,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * 현재 exception handler만 하는 RestControllerAdvice.
  * <p>
  * RuntimeException으로 통일. httpStatus가 401로 동일.
- * <li>5종의 예외 핸들링 중.
+ * <li> JsonProcessingException 핸들링 추가. http error status는 500
+ * <li>총 6종의 예외 핸들링 중.
  * </p>
  */
 @RestControllerAdvice
@@ -28,6 +30,12 @@ public class CommonAdvice {
       ExpiredJwtException.class})
   public ResponseEntity<CommonResponse<RuntimeException>> authorizationFailureHandler(RuntimeException e) {
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(CommonResponse.fail(e.getMessage()));
+  }
+
+  @ExceptionHandler(JsonProcessingException.class)
+  public ResponseEntity<CommonResponse<RuntimeException>> jsonProcessingExceptionHandler(JsonProcessingException e) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(CommonResponse.fail(e.getMessage()));
   }
 }
