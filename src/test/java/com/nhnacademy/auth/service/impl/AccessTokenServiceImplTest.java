60c99c274e3ef690af466235cb22c8e8e40a79e7
chanhwiim <chanhwiim459@gmail.com>
2024-04-06 18:48:10 +0900

    test: add DisplayName

--- src/test/java/com/nhnacademy/auth/service/impl/AccessTokenServiceImplTest.java
+++ src/test/java/com/nhnacademy/auth/service/impl/AccessTokenServiceImplTest.java
@@ -6,11 +6,11 @@ import com.nhnacademy.auth.domain.AccessToken;
 import com.nhnacademy.auth.service.AccessTokenService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Service;
 
 @Slf4j
 @SpringBootTest
@@ -32,11 +32,13 @@ class AccessTokenServiceImplTest {
 
   @Test
   @Order(2)
+  @DisplayName("access token find in mysql")
   void findAccessToken() {
     assertNotNull(accessTokenService.findAccessToken(accessToken));
   }
 
   @Test
+  @DisplayName("access token save in mysql")
   @Order(1)
   void saveAccessToken() {
     AccessToken accessToken1 = accessTokenService.saveAccessToken(accessToken, ip);
@@ -44,6 +46,7 @@ class AccessTokenServiceImplTest {
   }
 
   @Test
+  @DisplayName("access token update in mysql")
   @Order(3)
   void updateAccessToken() {
     accessTokenService.updateAccessToken(accessToken, newAccessToken);
@@ -51,8 +54,10 @@ class AccessTokenServiceImplTest {
   }
 
   @Test
+  @DisplayName("access token delete in mysql")
   @Order(4)
   void deleteAccessToken() {
     accessTokenService.deleteAccessToken(newAccessToken);
+    assertNull(accessTokenService.findAccessToken(accessToken));
   }
 }
