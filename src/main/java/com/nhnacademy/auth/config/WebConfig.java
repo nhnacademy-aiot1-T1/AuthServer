febb48b4976b3a9cffe06365ea74c8d72a58061b
ckddms6530 <ckddms6530@gmail.com>
2024-05-09 17:01:12 +0900

    refactor : resttemplate 빈 등록

--- src/main/java/com/nhnacademy/auth/config/WebConfig.java
+++ src/main/java/com/nhnacademy/auth/config/WebConfig.java
@@ -1,10 +1,13 @@
 package com.nhnacademy.auth.config;
 
-import com.nhnacademy.auth.interceptor.UserAgentInterceptor;
 import com.nhnacademy.auth.common.UserAgentStore;
+import com.nhnacademy.auth.interceptor.UserAgentInterceptor;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   private final UserAgentStore userAgentStore;
 
+
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(userAgentInterceptor())
@@ -28,4 +32,11 @@ public class WebConfig implements WebMvcConfigurer {
     return new UserAgentInterceptor(userAgentStore);
   }
 
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder
+        .setReadTimeout(Duration.ofSeconds(5L))
+        .setConnectTimeout(Duration.ofSeconds(5L))
+        .build();
+  }
 }
