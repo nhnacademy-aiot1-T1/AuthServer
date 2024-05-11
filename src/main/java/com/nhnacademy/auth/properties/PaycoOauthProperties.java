39e8e38adcf9f21eaa3e4199381fd2120871099d
ckddms6530 <ckddms6530@gmail.com>
2024-05-12 00:48:11 +0900

    refactor : Oauth2Properties 상속으로 생성자 및 Getter 삭제

--- src/main/java/com/nhnacademy/auth/properties/PaycoOauthProperties.java
+++ src/main/java/com/nhnacademy/auth/properties/PaycoOauthProperties.java
@@ -1,26 +1,21 @@
 package com.nhnacademy.auth.properties;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 /**
  * Payco Oauth 관련 프로퍼티.
  */
-@Getter
 @ConstructorBinding
-@AllArgsConstructor
 @ConfigurationProperties(prefix = "payco")
-public class PaycoOauthProperties {
+public class PaycoOauthProperties extends Oauth2Properties {
 
-  private final String clientId;
-  private final String clientSecret;
-  private final String tokenUrl;
-  private final String tokenPath;
-  private final String userInfoUrl;
-  private final String userInfoPath;
-
+  public PaycoOauthProperties(String clientId, String clientSecret, String tokenUrl,
+      String userInfoUrl) {
+    super(clientId, clientSecret, tokenUrl, userInfoUrl);
+  }
 }
+
+
 
 
