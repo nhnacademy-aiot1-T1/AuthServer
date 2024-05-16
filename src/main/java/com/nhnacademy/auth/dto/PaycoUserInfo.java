63018d8c03eeed1b3f7dd4f5877db9de4cd07522
jki12 <sjak0227@naver.com>
2024-05-16 10:49:51 +0900

    fix: getter, jsonCreator 어노테이션 추가

--- src/main/java/com/nhnacademy/auth/dto/PaycoUserInfo.java
+++ src/main/java/com/nhnacademy/auth/dto/PaycoUserInfo.java
@@ -1,17 +1,22 @@
 package com.nhnacademy.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.nhnacademy.auth.dto.domain.OauthUserInfo;
+import lombok.Getter;
+
 import java.util.Map;
 
 /**
  * Payco 사용자 정보.
  */
+@Getter
 public class PaycoUserInfo implements OauthUserInfo {
 
   private static final String MEMBER_KEY = "member";
 
   private final Map<String, Map<String, String>> data;
 
+  @JsonCreator
   public PaycoUserInfo(Map<String, Map<String, String>> data) {
     this.data = data;
   }
